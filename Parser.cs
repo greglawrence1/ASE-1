01494fd8eeed002269a73d29108fd9e981a1da64
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-12 14:36:07 +0000

    Rectangle class added which inherits from the shape class

--- Parser.cs
+++ Parser.cs
@@ -24,24 +24,32 @@ namespace ProperAssignment
         public void parseCommand(string command)
         {
             command.Trim().ToLower();
-            String[] hell = command.Split(' ');
-            if (hell[0] == "fillon")
+            String[] commands = command.Split(' ');
+            if (commands[0] == "fillon")
             {
                 Filled = true;
             }
-            if (hell[0] == "filloff")
+            if (commands[0] == "filloff")
             {
                 Filled = false;
             }
-            if (hell[0] == "circle")
+            if (commands[0] == "circle")
             {
-                int i = int.Parse(hell[1]);
-                int f = int.Parse(hell[2]);
-                int e = int.Parse(hell[3]);
+                int i = int.Parse(commands[1]);
+                int f = int.Parse(commands[2]);
+                int e = int.Parse(commands[3]);
                 Circle c = new Circle(Color.Blue, i, f, e, Filled);
                 c.draw(g);
             }
-
+            if (commands[0] == "rectangle")
+            {
+                int q1 = int.Parse(commands[1]);
+                int q2 = int.Parse(commands[2]);
+                int q3 = int.Parse(commands[3]);
+                int q4 = int.Parse(commands[4]);
+                Rectangle r = new Rectangle(Color.AliceBlue, q1, q2, q3, q4, Filled);
+                r.draw(g);
+            }
         }
     }
 }
