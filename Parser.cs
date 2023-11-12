b786deb8abe64e53308ecf22651f7a0ab6c97af2
Greg Lawrence <greg_lawrence2017@outlook.com>
2023-11-12 14:54:53 +0000

    triangle class added which inherits from the shape class

--- Parser.cs
+++ Parser.cs
@@ -50,6 +50,15 @@ namespace ProperAssignment
                 Rectangle r = new Rectangle(Color.AliceBlue, q1, q2, q3, q4, Filled);
                 r.draw(g);
             }
+            if (commands[0] == "triangle")
+            {
+                int t1 = int.Parse(commands[1]);
+                int t2 = int.Parse(commands[2]);
+                int t3 = int.Parse(commands[3]);
+
+                triangle t = new triangle(Color.AliceBlue, t1, t2, t3, Filled);
+                t.draw(g);
+            }
         }
     }
 }
---
+++ triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProperAssignment
+{
+    public class triangle : Shape
+    {
+
+        protected int z;
+        protected bool fill;
+        public triangle(Color colour, int x, int y, int z, bool fill) : base(colour, x, y)
+        {
+            this.z = z;
+            this.fill = fill;
+        }
+        public override void draw(Graphics g)
+        {
+            Pen p = new Pen(Color.Black, 2);
+            SolidBrush b = new SolidBrush(colour);
+            if (fill)
+            {
+                g.FillPolygon(b, new Point[] { new Point(x, y), new Point(y, z), new Point(z, x) });
+            }
+            else
+            {
+                g.DrawPolygon(p, new Point[] { new Point(x, y), new Point(y, z), new Point(z, x) });
+            }
+        }
+    }
+}
+    
+
